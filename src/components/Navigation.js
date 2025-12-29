f837042cfd11da7f3c2b600f17f6c5751600d704
JPrieve <10148119+JPrieve@users.noreply.github.com>
2025-12-29 09:14:48 -0600

    initial

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,7 +1,53 @@
+import { ethers } from 'ethers';
 import logo from '../assets/logo.svg';
 
 const Navigation = ({ account, setAccount }) => {
 
+    const connectHandler = async () => {
+    try {
+        if ( !window.ethereum ) {
+            alert('MetaMask not detected');
+            return;
+        }
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+        setAccount(accounts[0]);
+    } catch (error) {
+        console.error('MetaMask connection error:', error);
+        alert('Error connecting to MetaMask');
+    }
+    }
+
+    return (
+        <nav>
+            <ul className='nav__links'>
+                <li><a href="#">Buy</a></li>
+                <li><a href="#">Rent</a></li>
+                <li><a href="#">Sell</a></li>
+            </ul>
+
+            <div className='nav__brand'>
+                <img src={logo} alt="Logo" />
+                <h1>Millow</h1>
+            </div>
+
+            {account ? (
+                <button
+                    type="button"
+                    className='nav__connect'
+                >
+                    {account.slice(0, 6) + '...' + account.slice(38, 42)}
+                </button>
+            ) : (
+                <button
+                    type="button"
+                    className='nav__connect'
+                    onClick={connectHandler}
+                >
+                    Connect
+                </button>
+            )}
+        </nav>
+    );
 }
 
 export default Navigation;
